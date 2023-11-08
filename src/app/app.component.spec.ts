c396386304c1a5013e0580de3af9eb0a9afcf389
Carlos Galvan <carlos.galvan@ute-salta.com.ar>
2023-11-08 12:04:34 -0300

    video 87

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,3 +25,5 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('.content span')?.textContent).toContain('gifs-app app is running!');
   });
 });
+
+
---
+++ src/app/gifs/gifs.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home/home-page.component';
+import { SearchBoxComponent } from './components/search-box/shearch-box.component';
+import { NO_ERRORS_SCHEMA } from '@angular/compiler';
+import { HydrationFeatureKind } from '@angular/platform-browser';
+import { CardListCoponentComponent } from './components/Card-List/card-list.component/card-list.component';
+
+
+@NgModule({
+  declarations: [
+    HomePageComponent, SearchBoxComponent, CardListCoponentComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HomePageComponent,
+  ]
+})
+export class GifsModule { }
+
+
+
